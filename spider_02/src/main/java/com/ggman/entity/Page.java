74696fd31aed36e661ca80312d7aa46064e58730
GGmanys <1272178301@qq.com>
2019-05-17 11:20:22 +0800

    1.哈哈哈,就不说我干了嘛

--- spider_02/src/main/java/com/ggman/entity/Page.java
+++ spider_02/src/main/java/com/ggman/entity/Page.java
@@ -4,5 +4,18 @@ package com.ggman.entity;
  * 保存页面信息
  */
 public class Page {
-
+    //页面内容
+    private String content;
+    //总播放量
+    private String allnumber;
+    //每日播放量
+    private String daynumber;
+    //评论数
+    private String commentnnumber;
+    //收藏数
+    private String  conllectnumber;
+    //赞
+    private String suppornumber;
+    //踩
+    private String againstnumber;
 }
